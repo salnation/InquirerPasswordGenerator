af0f7bab8b81c26826f1f02fdb2b1778c95e2b8f
AC <adrianacovic@AC.local>
2020-07-28 06:38:58 -0400

    second commit - some variables have been set and defined and the asyncronous javascript funstion has been created.

--- index.js
+++ index.js
@@ -9,3 +9,15 @@ const fs = require('fs');
 const path = require('path');
 
 //The basic functionality of require is that it reads a JavaScript file, executes the file, and then proceeds to return the exports object.
+
+async function main() {
+    // the await keyword is permitted within them. The async and await keywords enable asynchronous, promise-based behavior to be written in a cleaner style
+    console.log('Start');
+    const userResponse = await iquirer 
+    // The prompt() method displays a dialog box that prompts the visitor for input.
+    .prompt([
+        
+    ])
+}
+
+//An async function is a function declared
