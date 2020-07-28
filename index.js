b617d540dcb6481387937b3ee955088445c8cb8b
AC <adrianacovic@AC.local>
2020-07-28 06:28:48 -0400

    first commit - added license, ReadMe from github and created index.js file to begin.

---
+++ index.js
@@ -0,0 +1,11 @@
+// Let's start by creating some variables to begin 
+
+const axios = require("axios");
+const inquirer = require("inquirer");
+
+//the builtin require function is the easiest way to include modules that exist in separate files. 
+
+const fs = require('fs');
+const path = require('path');
+
+//The basic functionality of require is that it reads a JavaScript file, executes the file, and then proceeds to return the exports object.
